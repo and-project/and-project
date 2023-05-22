edf90c34fbf03066735feff0e20ef5a97f2d6890
shinyeongwoon <duddns119@naver.com>
2023-05-22 13:01:24 +0900

    AP - 64 Access-Control-Allow-Origin Setting
    filter bean name change

--- backend/src/main/java/com/fx/and_proejct/filter/CORSFilter.java
+++ backend/src/main/java/com/fx/and_proejct/filter/CORSFilter.java
@@ -8,13 +8,14 @@ import java.io.IOException;
 
 @Component
 public class CORSFilter implements Filter {
+
     @Override
-    public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws IOException, ServletException {
-        HttpServletResponse response = (HttpServletResponse) resp;
-        response.setHeader("Access-Control-Allow-Origin", "*");
-        response.setHeader("Access-Control-Allow-Methods", "POST, GET, DELETE, PUT");
-        response.setHeader("Access-Control-Max-Age", "1000");
-        response.setHeader("Access-Control-Allow-Headers", "x-requested-with, origin, content-type, accept");
-        chain.doFilter(req, resp);
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+        httpResponse.setHeader("Access-Control-Allow-Origin", "*");
+        httpResponse.setHeader("Access-Control-Allow-Methods", "POST, GET, DELETE, PUT");
+        httpResponse.setHeader("Access-Control-Max-Age", "1000");
+        httpResponse.setHeader("Access-Control-Allow-Headers", "x-requested-with, origin, content-type, accept");
+        chain.doFilter(request, response);
     }
 }
