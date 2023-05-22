1508451f2de422370e6c7bd6bda93cd30a68dd30
shinyeongwoon <duddns119@naver.com>
2023-05-22 15:38:31 +0900

    AP - 64 Access-Control-Allow-Origin Setting
    test date update & paging update

--- backend/src/main/java/com/fx/and_proejct/WebConfig.java
+++ backend/src/main/java/com/fx/and_proejct/WebConfig.java
@@ -2,15 +2,17 @@ package com.fx.and_proejct;
 
 import com.fx.and_proejct.filter.CORSFilter;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcRegistrations;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.servlet.Filter;
 
 @Slf4j
 @Configuration
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
 
     @Bean
     public FilterRegistrationBean corsSetFilter(){
