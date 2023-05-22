3a1ada0fce724d1d48a53a321443693efee99b36
shinyeongwoon <duddns119@naver.com>
2023-05-22 12:53:40 +0900

    AP - 64 Access-Control-Allow-Origin Setting
    testing

--- backend/src/main/java/com/fx/and_proejct/WebConfig.java
+++ backend/src/main/java/com/fx/and_proejct/WebConfig.java
@@ -1,12 +1,14 @@
 package com.fx.and_proejct;
 
 import com.fx.and_proejct.filter.CORSFilter;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import javax.servlet.Filter;
 
+@Slf4j
 @Configuration
 public class WebConfig {
 
@@ -15,7 +17,8 @@ public class WebConfig {
         FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
         filterRegistrationBean.setFilter(new CORSFilter());
         filterRegistrationBean.setOrder(0);
-        filterRegistrationBean.addUrlPatterns("/*");
+        log.info("in process");
+        filterRegistrationBean.addUrlPatterns("*");
         return filterRegistrationBean;
     }
 
