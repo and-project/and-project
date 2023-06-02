1ec7e3f87bad7d180b6442daa3faff09702f266f
number9135 <navigate0425@gmail.com>
2023-06-02 19:36:58 +0900

    DrawerLayout

--- frontend/src/component/form/DrawerLayout.js
+++ frontend/src/component/form/DrawerLayout.js
@@ -1,34 +1,84 @@
 import React from 'react'
 import {View, Text, SafeAreaView, TouchableOpacity, StyleSheet} from 'react-native'
-import { MaterialIcons} from '@expo/vector-icons';
-import { Entypo } from '@expo/vector-icons';
-import { MaterialCommunityIcons } from '@expo/vector-icons';
-
+import { MaterialCommunityIcons, MaterialIcons, Entypo } from '@expo/vector-icons';
 
 export default function DrawerLayout() {
   return (
     <SafeAreaView style={{ flex: 1, borderWidth: 1, width: 350 }}>
       <View
         style={{
-          borderWidth: 1,
-          height: 100,
+          flex: 1,
+          flexDirection: "row",
+          alignItems: "center",
+          marginLeft: 10,
+          marginTop: 40,
+        }}
+      >
+        <View
+          style={{
+            borderWidth: 1,
+            height: 75,
+            width: 75,
+            justifyContent: "center",
+            alignItems: "center",
+            backgroundColor: "gray",
+            opacity: 0.5,
+            borderRadius: 15,
+          }}
+        >
+          <MaterialCommunityIcons
+            name="account-alert-outline"
+            size={50}
+            color="black"
+          />
+        </View>
+        <View style={{ flexDirection: "column", paddingTop: 15 }}>
+          <Text style={{ fontSize: 20, marginLeft: 10 }}>비입주자</Text>
+          <Text style={{ fontSize: 20, marginLeft: 10 }}>
+            로그인을 해주세요.
+          </Text>
+        </View>
+      </View>
+
+      <View
+        style={{
+          borderWidth: 0,
+          borderRadius: 20 / 2,
+          backgroundColor: "gainsboro",
+          opacity: 1,
+          width: 330,
+          height: 50,
           marginTop: 10,
           flexDirection: "row",
           alignItems: "center",
           justifyContent: "space-around",
+          alignSelf: "center",
         }}
       >
-        <TouchableOpacity style={styles.signInButton}>
-          <Text>로그인</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.signInButton}>
-          <Text>회원가입</Text>
-        </TouchableOpacity>
+        <View style = {{borderRightWidth: 1, flex:1, justifyContent:'center', alignItems:'center', height:30,}}>
+          <TouchableOpacity style={styles.signInButton}>
+            <Text style={{ fontSize: 17 }}>로그인</Text>
+          </TouchableOpacity>
+        </View>
+        <View style = {{flex:1, justifyContent:'center', alignItems:'center', height: 30,}}>
+          <TouchableOpacity style={styles.signInButton}>
+            <Text style={{ fontSize: 17 }}>회원가입</Text>
+          </TouchableOpacity>
+        </View>
       </View>
 
-      <View style={{ flex: 3, borderWidth: 1, alignItems: "center" }}>
-        <View style={{justifyContent:'center', alignItems:'center'}}>
-          <Text style = {{fontSize:15, marginTop:10, borderWidth:1,}}>일반기능</Text>
+      <View
+        style={{
+          borderTopWidth: 1,
+          marginTop: 10,
+          alignItems: "center",
+          height: 300,
+        }}
+      >
+        <View style={{ justifyContent: "center", alignItems: "center" }}>
+          <Text style={{ fontSize: 15, marginTop: 10, borderWidth: 1 }}>
+            일반기능
+          </Text>
         </View>
 
         <View
@@ -37,7 +87,7 @@ export default function DrawerLayout() {
             borderWidth: 1,
             justifyContent: "space-around",
             alignItems: "center",
-            marginTop: 30,
+            marginTop: 20,
             height: 70,
             width: 350,
           }}
@@ -69,8 +119,7 @@ export default function DrawerLayout() {
       </View>
 
       <View style={{ flex: 4, borderWidth: 1, backgroundColor: "gray" }}>
-
-      <View
+        <View
           style={{
             flexDirection: "row",
             borderWidth: 1,
@@ -113,9 +162,9 @@ export default function DrawerLayout() {
 
 const styles = StyleSheet.create({
   signInButton : {
-    borderWidth: 1,
-    height: 50,
-    width: 120,
+    borderWidth: 0,
+    height: 40,
+    width: 110,
     borderRadius: 20,
     justifyContent: 'center',
     alignItems: 'center'
